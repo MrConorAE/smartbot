84bdf6b0f19432af0edd8d4dcdc4ff8b93ef557e
MrConorAE <26195325+MrConorAE@users.noreply.github.com>
2020-06-30 14:38:03 +1200

    Added the changelog, help and stat commands.

--- index.js
+++ index.js
@@ -19,32 +19,25 @@ client.on('ready', () => {
 	console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('message', msg => {
-	if (msg.content === "ping") {
-		msg.channel.send("pong (☞ﾟヮﾟ)☞");
-	}
-});
-
 client.on('ready', () => {
 	console.log(`Logged in as ${client.user.tag}!`);
 	let ch = client.channels.cache.get(config.channels.log);
-	//ch.setTopic("Last restart: " + client.readyAt + " - Version: v" + config.ver);
 	client.user.setActivity(config.activity.content, {
 		type: config.activity.type
 	});
 	ch.send({
 		embed: {
-			color: 0x31d400,
+			color: 0x03c129,
 			author: {
 				name: client.user.username,
 				icon_url: client.user.avatarURL
 			},
-			title: "Bot online!",
+			title: "Connected!",
 			description: (`Version: ${config.ver} (use %changelog to see what's new)`),
 			timestamp: new Date(),
 			footer: {
 				icon_url: client.user.avatarURL,
-				text: "Automated message"
+				text: "Automated message."
 			}
 		}
 	});
@@ -52,8 +45,116 @@ client.on('ready', () => {
 
 client.on('message', msg => {
 	try {
-		messages = messages + 1;
-		// Commands begin here. //
+		messages = messages + 1; // Add one to the messages count.
+		// COMMANDS BEGIN HERE
+		if (msg.content === "%stat") {
+			// Reply with the statistics since the last restart.
+			commands = commands + 1;
+			msg.channel.send({
+				embed: {
+					color: 0x03b8c1,
+					author: {
+						name: client.user.username,
+						icon_url: client.user.avatarURL
+					},
+					title: "Statistics",
+					description: "Statistics since the last restart.",
+					fields: [{
+							name: "Messages",
+							value: messages
+						},
+						{
+							name: "Commands",
+							value: commands
+						},
+						{
+							name: "Uptime",
+							value: client.uptime
+						}
+					],
+					timestamp: new Date(),
+					footer: {
+						icon_url: client.user.avatarURL,
+						text: "Channel: " + msg.channel.name + " - User: " + msg.author.username
+					}
+				}
+			});
+		} else if (msg.content === "%help") {
+			// Reply with the help message.
+			commands = commands + 1;
+			msg.channel.send("beep boop here is help");
+			msg.channel.send({
+				embed: {
+					color: 0x03b8c1,
+					author: {
+						name: client.user.username,
+						icon_url: client.user.avatarURL
+					},
+					title: "Help",
+					description: "Here's a list of commands. Some of these require the Bot Commander role.\n\nKey: <...> = argument, (1/2/3/...) = options, @user = mention, [argument] = optional argument",
+					fields: [{
+							name: "General",
+							value: "%stat, %help, %changelog"
+						},
+						{
+							name: "Moderation",
+							value: "%clear <n>, %kick <@user>, %ban <@user>\n%mute <@user>, %tempmute <hh:mm> <@user>, %unmute <@user>"
+						},
+						{
+							name: "Tools",
+							value: "%mood <type (w/s/l/p)> <text>, %list <(admins)>, %poll <(b/2/5)> <text>"
+						},
+						{
+							name: "Easter Eggs",
+							value: "SmartBot also has hidden Easter eggs! Try to find them all."
+						}
+					],
+					timestamp: new Date(),
+					footer: {
+						icon_url: client.user.avatarURL,
+						text: "Channel: " + msg.channel.name + " - User: " + msg.author.username
+					}
+				}
+			});
+		} else if (msg.content === "%changelog") {
+			// Reply with the changelog.
+			commands = commands + 1;
+			msg.channel.send({
+				embed: {
+					color: 0x03b8c1,
+					author: {
+						name: client.user.username,
+						icon_url: client.user.avatarURL
+					},
+					title: "Changelog for version " + config.ver,
+					description: "Here's the changelog for the last update, telling you what's new, different or removed.",
+					fields: [{
+							name: "New",
+							value: "Everything. Just check %help."
+						},
+						{
+							name: "Changed",
+							value: "--"
+						},
+						{
+							name: "Removed",
+							value: "--"
+						},
+						{
+							name: "Complaints? Suggestions? Bugs?",
+							value: "Tell MrConorAE in #smartbot-suggestions with your message."
+						}
+					],
+					timestamp: new Date(),
+					footer: {
+						icon_url: client.user.avatarURL,
+						text: "Channel: " + msg.channel.name + " - User: " + msg.author.username
+					}
+				}
+			});
+		} else if (msg.content === "%ping") {
+			msg.channel.send("Pong (☞ﾟヮﾟ)☞");
+		}
 	} catch (e) {
 		let ch = client.channels.cache.get(config.channels.log);
 		ch.send({
